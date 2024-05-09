38e81b0fc5200083ae6c9374566545599ef80439
anikaet7 <124033088+anikaet7@users.noreply.github.com>
2024-05-09 20:58:17 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ import path from 'path';
 dotenv.config();
 
 mongoose
-    .connect('mongodb://localhost:27017')
+    .connect(process.env.MONGO)
     .then(() => {
         console.log('Connected to MongoDB');
     })
